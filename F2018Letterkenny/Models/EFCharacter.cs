ea0e3174f97e44452359b32688ca05a6bdc41a27
hiendieulam <200381290@student.georgianc.on.ca>
2018-12-14 09:38:19 -0500

    test

---
+++ F2018Letterkenny/Models/EFCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace F2018Letterkenny.Models
+{
+    public class EFCharacter : IMockCharacter
+    {
+        private DatabaseModel db = new DatabaseModel();
+        //public IQueryable<Characters> IMockCharacter.Characters { get { return db.Characters;  } }
+        public IQueryable<Character> Characters { get { return db.Characters; } }
+        Character IMockCharacter.Save(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
